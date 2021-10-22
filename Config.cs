3e89e1e5066e0bd6be9aa200c0488530756774e3
workwhileweb <workwhileweb@gmail.com>
2021-10-23 01:16:55 +0700

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿namespace Public
+{
+    public class Config
+    {
+        public string DestinationExe { get; }
+
+        public static Config Load(string file)
+        {
+
+            return new Config();
+        }
+        public Config() { }
+        public Config(string destinationExe)
+        {
+            DestinationExe = destinationExe;
+        }
+    }
+}
